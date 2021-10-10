ddcbaa10a66e91264bd701643276984c5a94555a
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-10-10 12:27:43 +0530

    Technologies section done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have worked with different technologies in the web development world.
+      Started with React and wish to explore more. In terms of Machine Learning
+      I use Python and Scikit-Learn and have built a recent interest towards
+      AutoML by H20.ai and Pycaret.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle> Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br /> React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle> Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br /> Node.js and MongoDB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle> UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br /> Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
